e41ce615184c2be424e890295a39100196d42d73
Tristan Nathanael Gautama <tristangautamaa@gmail.com>
2025-05-23 20:14:12 +0700

    Finished chatbot and hidden API key

--- lib/chatbot_page.dart
+++ lib/chatbot_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ai_toolkit/flutter_ai_toolkit.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
+import 'home_page.dart';
 
 class ChatbotPage extends StatelessWidget {
   const ChatbotPage({super.key});
@@ -8,14 +9,37 @@ class ChatbotPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => const HomePage()),
+            );
+          },
+        ),
+        title: const Text(
+          'ZEAT AI',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.brown[700],
+      ),
+      backgroundColor: Colors.grey[850],
       body: LlmChatView(
         provider: GeminiProvider(
           model: GenerativeModel(
-            model: 'gemini-2.0-flash',
-            apiKey:
-                'AIzaSyBCe315zyhyhcBQ-8xAKGsaDnWQV60RZfg', // Replace with your actual API key or import from secrets
+            model: 'gemini-1.5-flash',
+            apiKey: 'AIzaSyBCe315zyhyhcBQ-8xAKGsaDnWQV60RZfg',
           ),
         ),
+        welcomeMessage:
+            'Hello! I am your pastry chatbot. How can I assist you today?',
       ),
     );
   }
--- lib/secrets.dart
+++ lib/secrets.dart
@@ -1,4 +1,5 @@
 class Secrets {
   static const String googleMapsApiKey =
       'AIzaSyBDc2HaJmoMetyszbBdprN2pL1uylj8iFc';
+  static const geminiApiKey = 'AIzaSyBCe315zyhyhcBQ-8xAKGsaDnWQV60RZfg';
 }
